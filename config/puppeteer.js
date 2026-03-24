6024f759bd803386f93fb36dbb63c031fa3219a0
Krish Radadiya <krishradadiya19@gmail.com>
2026-03-24 16:56:05 +0530

    remove useless things

--- config/puppeteer.js
+++ config/puppeteer.js
@@ -2,19 +2,9 @@ const puppeteer = require("puppeteer");
 
 async function launchBrowser() {
     return await puppeteer.launch({
-        headless: true,
-        browser: 'chromium',
-        cacheDirectory: '/opt/render/.cache/puppeteer',
+        headless: false,
         defaultViewport: null,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-first-run",
-            "--no-zygote",
-            "--disable-gpu"
-        ],
+        args: ["--start-maximized"],
     });
 }
 
